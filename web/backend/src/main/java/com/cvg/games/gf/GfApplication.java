711ac74e2aa97a7f819eb5f08e296e3e3e7248b2
Cristhian Villegas <cristianvg9692@gmail.com>
2023-05-24 13:45:22 -0600

    Springboot backend for this branch

---
+++ web/backend/src/main/java/com/cvg/games/gf/GfApplication.java
@@ -0,0 +1,13 @@
+package com.cvg.games.gf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfApplication.class, args);
+	}
+
+}
---
+++ web/backend/src/test/java/com/cvg/games/gf/GfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvg.games.gf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
